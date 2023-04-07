42cc0d4a45772cd7685f9b60f96a5b53fb30cad4
NantawatCharoenrat <vss555mos@gmail.com>
2023-04-07 12:43:29 +0700

    07/03/2566

---
+++ student.sql
@@ -0,0 +1,11 @@
+CREATE TABLE student (
+    student_id Varchar (13) NOT NULL ,
+    student_pass Varchar (30) ,
+    name Varchar (100) ,
+    lastname Varchar (100) ,
+    year Int ,
+    term Int ,
+    email Varchar (100) ,
+    phone Varchar (10) ,
+    PRIMARY KEY (student_id)
+)
