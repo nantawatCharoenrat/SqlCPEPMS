c0f458e91b9c6916b7d706349450487ccd0bd121
nantawatCharoenrat <vss555mos@gmail.com>
2023-07-05 14:34:20 +0700

    05/07/2566

--- insertStudent61-65/Student62.sql
+++ insertStudent61-65/Student62.sql
@@ -1,233 +1,233 @@
 -- ระบบบริหารจัดการร้านเสริมสวยสำหรับร้านแฮร์ทูแฮร์ จังหวัดชลบุรี
-insert into student
-    values ("1158304620359","620359","ณัฐกิจ","คำสีแก้ว",2562,1,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158304620359","620359","ณัฐกิจ","คำสีแก้ว",2562,1);
 
-insert into student
-    values ("1158304620227","620227","จันทรา","ทิมแก้ว",2562,1,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158304620227","620227","จันทรา","ทิมแก้ว",2562,1);
 
-insert into student
-    values ("1158304620235","620235","เกียรติคุณ","จำนงอุดม",2562,1,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158304620235","620235","เกียรติคุณ","จำนงอุดม",2562,1);
 
 -- ระบบบริหารจัดการงานพัฒนาซอฟต์แวร์ผ่านเว็บ กรณีศึกษา บริษัท ทูเฟลโลส์ เน็ตเวิร์ค แอนด์ ดีไซน์ จากัด
-insert into student
-    values ("1159304620019","620019","พัฒนพล","มุสิกนันท์",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159304620019","620019","พัฒนพล","มุสิกนันท์",2562,2);
 
-insert into student
-    values ("1159304620373","620373","เกียรติยศ","สุนทรกนกพงศ์",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159304620373","620373","เกียรติยศ","สุนทรกนกพงศ์",2562,2);
 
 -- ระบบจัดการการเรียนการสอนสำหรับคอร์สออนไลน์
-insert into student
-    values ("1159104000792","000792","ไกรศร","ยิ้มเยื้อน",2562,1,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159104000792","000792","ไกรศร","ยิ้มเยื้อน",2562,1);
 
-insert into student
-    values ("1159104000503","000503","พงศา","อัฐวงศ์",2562,1,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159104000503","000503","พงศา","อัฐวงศ์",2562,1);
 
-insert into student
-    values ("1159104000446","000446","วรพล","จีนนะฮู้",2562,1,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159104000446","000446","วรพล","จีนนะฮู้",2562,1);
 
 -- ระบบตรวจสอบคุณภาพการพิมพ์ตัวอักษรบนซองบรรจุผลิตภัณฑ์
-insert into student
-    values ("1158104000182","000182","ประมาณ","มูลประสาน",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104000182","000182","ประมาณ","มูลประสาน",2562,2);
 
-insert into student
-    values ("1158104620518","620518","จักรกริช","สิงห์เรือง",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620518","620518","จักรกริช","สิงห์เรือง",2562,2);
     
-insert into student
-    values ("1158104620047","620047","จิรนุช","บุดดา",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620047","620047","จิรนุช","บุดดา",2562,2);
 
 -- ระบบช่วยงานสารบรรณและทำแฟ้มสะสมงานของบุคลากรภาควิชาวิศวกรรมคอมพิวเตอร์ รุ่นที่ 2
-insert into student
-    values ("1159104000206","000206","พรพิพัฒน์","กลิ่นทรัพย์",2562,1,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159104000206","000206","พรพิพัฒน์","กลิ่นทรัพย์",2562,1);
 
-insert into student
-    values ("1159104620458","620458","สหรัฐ","นิ่มถนอม",2562,1,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159104620458","620458","สหรัฐ","นิ่มถนอม",2562,1);
 
-insert into student
-    values ("1159104002103","002103","วริชภัทร","แก้วสุริยาภรณ์",2562,1,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159104002103","002103","วริชภัทร","แก้วสุริยาภรณ์",2562,1);
 
 -- ระบบตรวจจับวัตถุด้วยหลักการการเรียนรู้ของเครื่อง
-insert into student
-    values ("1158104620583","620583","กำธร","คำหล่อ",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620583","620583","กำธร","คำหล่อ",2562,2);
 
-insert into student
-    values ("1158104620872","620872","อธิภู","อัครปรีดี",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620872","620872","อธิภู","อัครปรีดี",2562,2);
 
 -- ระบบแบบคำร้องออนไลน์ กรณีศึกษา คณะวิศวกรรมศาสตร์ มหาวิทยาลัยเทคโนโลยีราชมงคลธัญบุรี
-insert into student
-    values ("1159304620233","620233","นัชพล","นนทศิลา",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159304620233","620233","นัชพล","นนทศิลา",2562,2);
 
-insert into student
-    values ("1159304620241","620241","วรรณกร","พรมรักษ์",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159304620241","620241","วรรณกร","พรมรักษ์",2562,2);
 
 -- โปรแกรมประยุกต์บนสมาร์ตโฟน “ขอไปด้วยช่วยโลก”
-insert into student
-    values ("1158104620161","620161","ธนภูมิ","บับพาน",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620161","620161","ธนภูมิ","บับพาน",2562,2);
 
-insert into student
-    values ("1158104620385","620385","มานิต","อิทธิโชติ",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620385","620385","มานิต","อิทธิโชติ",2562,2);
 
 -- ระบบตรวจสอบบุคคลภายในอาคารโดยใช้การประมวลผลภาพ
-insert into student
-    values ("1159304620118","620118","ณรชต","ประเสริฐสุข",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159304620118","620118","ณรชต","ประเสริฐสุข",2562,2);
 
-insert into student
-    values ("1159304620134","620134","ปัญญาพร","กิ่งพวง",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159304620134","620134","ปัญญาพร","กิ่งพวง",2562,2);
 
-insert into student
-    values ("1159304620142","620142","ธนศักดิ์","จันทรโสภณ",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159304620142","620142","ธนศักดิ์","จันทรโสภณ",2562,2);
 
 -- ระบบตรวจสอบการใช้งานห้องในอาคารสำนักงาน
-insert into student
-    values ("1158104620294","620294","กฤษฎา","วงศ์ศร",2562,1,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620294","620294","กฤษฎา","วงศ์ศร",2562,1);
 
-insert into student
-    values ("1158104620328","620328","ศรัณย์ชัย","สำอางศรี",2562,1,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620328","620328","ศรัณย์ชัย","สำอางศรี",2562,1);
 
-insert into student
-    values ("1158104620633","620633","สัณหรัฐฐ์","สวัสดิยากร",2562,1,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620633","620633","สัณหรัฐฐ์","สวัสดิยากร",2562,1);
 
 -- โปรแกรมจัดหมวดหมู่เอกสารการประกันคุณภาพการศึกษาภายใน ระดับอุดมศึกษา พ.ศ. 2557
-insert into student
-    values ("1159304620217","620217","กิตติพจน์","วงษ์ถาวรเรือง",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159304620217","620217","กิตติพจน์","วงษ์ถาวรเรือง",2562,2);
 
-insert into student
-    values ("1159304620308","620308","ชุติวัฒณ์","ก่อแก้ว",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159304620308","620308","ชุติวัฒณ์","ก่อแก้ว",2562,2);
 
 -- ระบบสารสนเทศเพื่อการจัดการงานรับส่งนักเรียน
-insert into student
-    values ("1158104620146","620146","ศิราณี","ลุนพุฒ",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620146","620146","ศิราณี","ลุนพุฒ",2562,2);
 
-insert into student
-    values ("1158104620773","620773","สหรัฐ","สุดเจริญ",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620773","620773","สหรัฐ","สุดเจริญ",2562,2);
 
-insert into student
-    values ("1158104620807","620807","เปี่ยมทรัพย์","ปราณีธรรม",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620807","620807","เปี่ยมทรัพย์","ปราณีธรรม",2562,2);
 
 -- แอปพลิเคชันสื่อการฝึกทักษะการเรียนรู้ของเด็กแอลดี
-insert into student
-    values ("1158104620237","620237","ศักดิ์ดา","พลายแก้ว",2562,1,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620237","620237","ศักดิ์ดา","พลายแก้ว",2562,1);
 
-insert into student
-    values ("1158104620823","620823","นครินทร์","ธิราชัย",2562,1,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620823","620823","นครินทร์","ธิราชัย",2562,1);
 
 -- ระบบควบคุมการเข้าอาคารด้วยเทคนิคการรู้จำใบหน้า
-insert into student
-    values ("1159104003879","003879","รัฐพงศ์","จันทร์สุข",2562,1,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159104003879","003879","รัฐพงศ์","จันทร์สุข",2562,1);
 
-insert into student
-    values ("1159104620078","620078","ณัฏฐเมศฐ์","วงค์วิไล",2562,1,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159104620078","620078","ณัฏฐเมศฐ์","วงค์วิไล",2562,1);
 
 -- ระบบสารสนเทศรถตู้โดยสารประจำทาง
-insert into student
-    values ("1158104620203","620203","วรเทพ","เลิศเกียรติวงศ์",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620203","620203","วรเทพ","เลิศเกียรติวงศ์",2562,2);
 
-insert into student
-    values ("1158104620591","620591","เจษฎากร","อยู่สว่าง",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620591","620591","เจษฎากร","อยู่สว่าง",2562,2);
 
-insert into student
-    values ("1158104620435","620435","ชลดา","แสนสุภา",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620435","620435","ชลดา","แสนสุภา",2562,2);
 
 -- ระบบบริหารจัดการเต้ารับด้วยรหัสคิวอาร์
-insert into student
-    values ("1158104620013","620013","ศุภณัฐ","แพรกปาน",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620013","620013","ศุภณัฐ","แพรกปาน",2562,2);
 
-insert into student
-    values ("1158104620062","620062","เวธกา","จันทร์เฑียร",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620062","620062","เวธกา","จันทร์เฑียร",2562,2);
 
-insert into student
-    values ("1158104620450","620450","ณัฐชัย","โสตถิพันธ์",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620450","620450","ณัฐชัย","โสตถิพันธ์",2562,2);
 
 -- ระบบแจ้งเตือนการโจรกรรมรถจักรยานยนต์และอุบัติเห
-insert into student
-    values ("1159304620381","620381","พีรพลพล","ขาวซัง",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159304620381","620381","พีรพลพล","ขาวซัง",2562,2);
 
-insert into student
-    values ("1159304620175","620175","สุกัญญา","จันทร์ประสิทธิ์",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159304620175","620175","สุกัญญา","จันทร์ประสิทธิ์",2562,2);
 
 -- ระบบบริหารจัดการวารสาร วิชาการออนไลน์
-insert into student
-    values ("1159404620109","620109","หริศ","พึ่งรัตนา",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159404620109","620109","หริศ","พึ่งรัตนา",2562,2);
 
-insert into student
-    values ("1159404620125","620125","อัศชนะ","คูเจริญไพบูลย์",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159404620125","620125","อัศชนะ","คูเจริญไพบูลย์",2562,2);
 
 -- ระบบตรวจสอบสถานะของนักศึกษาด้วยบาร์โค้ดผ่านแอนดรอยด์แอปพลิเคชัน
-insert into student
-    values ("1158304620102","620102","รัฐพงษ์","ฐติกุล",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158304620102","620102","รัฐพงษ์","ฐติกุล",2562,2);
 
-insert into student
-    values ("1158304620169","620169","อมรเทพ","ชาวบุตรดี",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158304620169","620169","อมรเทพ","ชาวบุตรดี",2562,2);
 
 -- แอปพลิเคชันนับจำนวนเกลียวของเส้นด้ายบนโทรศัพท์มือถือ
-insert into student
-    values ("1158104620351","620351","ศิวะ","การเจริญพงศ์",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620351","620351","ศิวะ","การเจริญพงศ์",2562,2);
 
-insert into student
-    values ("1158104620609","620609","ธนาคาร","หลักแหลม",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620609","620609","ธนาคาร","หลักแหลม",2562,2);
 
-insert into student
-    values ("1158104620757","620757","พรชัย","ทองดี",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620757","620757","พรชัย","ทองดี",2562,2);
 
 -- ระบบจองห้องผ่านเว็บ สำหรับคณะบริหารธุรกิจ มหาวิทยาลัยเทคโนโลยีราชมงคลธัญบุรี
-insert into student
-    values ("1159404620166","620166","กัมปนาท","แคนเพชร",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159404620166","620166","กัมปนาท","แคนเพชร",2562,2);
 
-insert into student
-    values ("1159404620026","620026","วินิจ","พงษ์เผื่อน",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159404620026","620026","วินิจ","พงษ์เผื่อน",2562,2);
 
 -- ระบบเช็คชื่อโดยการจดจำใบหน้า
-insert into student
-    values ("1158104620054","620054","อนุรักษ์","ไชยบุญ",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620054","620054","อนุรักษ์","ไชยบุญ",2562,2);
 
-insert into student
-    values ("1158104620336","620336","พรหมพักตร์","สินบุญญานนท์",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620336","620336","พรหมพักตร์","สินบุญญานนท์",2562,2);
 
-insert into student
-    values ("1158104620765","620765","อภิวัฒน์","นามบุตร",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620765","620765","อภิวัฒน์","นามบุตร",2562,2);
 
 -- เว็บแอปพลิเคชันสำหรับการเรียนรู้ไวยากรณ์ภาษาอังกฤษระดับชั้นประถมศึกษา
-insert into student
-    values ("1159304620266","620266","สิทธวีร์","รุ่งโรจน์ศิริกุล",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159304620266","620266","สิทธวีร์","รุ่งโรจน์ศิริกุล",2562,2);
 
-insert into student
-    values ("1159304620357","620357","รัชฎาภรณ์","ขำเหมือน",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1159304620357","620357","รัชฎาภรณ์","ขำเหมือน",2562,2);
 
 -- ระบบตรวจสอบดูแลบ้านอัตโนมัติ
-insert into student
-    values ("1158104000174","000174","ชาญณรงค์","จันทร์แก้ว",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104000174","000174","ชาญณรงค์","จันทร์แก้ว",2562,2);
 
-insert into student
-    values ("1158104620625","620625","อเนก","ปิ่นชัยศิริ",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620625","620625","อเนก","ปิ่นชัยศิริ",2562,2);
 
-insert into student
-    values ("1158104620690","620690","พงศ์สวัสดิ์","จันทร์จินดา",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620690","620690","พงศ์สวัสดิ์","จันทร์จินดา",2562,2);
 
 -- โปรแกรมควบคุมตู้เก็บของผ่านมือถือ
-insert into student
-    values ("1158104620286","620286","ธีรชัย","ผดุงพล",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620286","620286","ธีรชัย","ผดุงพล",2562,2);
 
-insert into student
-    values ("1158104620526","620526","พัชรพงษ์","เนียมประชา",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620526","620526","พัชรพงษ์","เนียมประชา",2562,2);
 
-insert into student
-    values ("1158104620815","620815","กฤษดา","มีสกุล",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620815","620815","กฤษดา","มีสกุล",2562,2);
 
 -- ระบบฐานข้อมูลงานวิจัย
-insert into student
-    values ("1158104620708","620708","ประพันธ์","ประทุมวัน",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620708","620708","ประพันธ์","ประทุมวัน",2562,2);
 
-insert into student
-    values ("1158104620492","620492","ณัฏฐ์","วงศ์ยุติธรรม",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620492","620492","ณัฏฐ์","วงศ์ยุติธรรม",2562,2);
 
-insert into student
-    values ("1158104000323","000323","อนิรุทธิ์","โสภารักษ์",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104000323","000323","อนิรุทธิ์","โสภารักษ์",2562,2);
 
 -- โรงเพาะเห็ดขนาดเล็กควบคุมผ่านเว็บแอปพลิเคชัน
-insert into student
-    values ("1158104620021","620021","เอกภพ","แก้ววันนา",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620021","620021","เอกภพ","แก้ววันนา",2562,2);
 
-insert into student
-    values ("1158104620318","620318","ณัฐพงศ์","ลือชัย",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620318","620318","ณัฐพงศ์","ลือชัย",2562,2);
 
-insert into student
-    values ("1158104620310","620310","ธวัชชัย","ศิรินาคสมบูรณ์",2562,2,"null","null");
+insert into student(student_id, student_password, firstname, lastname, year, term)
+    values ("1158104620310","620310","ธวัชชัย","ศิรินาคสมบูรณ์",2562,2);
