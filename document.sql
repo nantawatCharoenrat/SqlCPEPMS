2e1053c8d1cdff1c17ce89d93f0a023f4e72a960
nantawat charoenrat <vss555mos@gmail.com>
2023-07-19 21:50:41 +0700

    19/07/2023

---
+++ document.sql
@@ -0,0 +1,10 @@
+-- 5 new
+CREATE TABLE document (
+    document_id Int NOT NULL AUTO_INCREMENT,
+    document_path Mediumtext ,
+    document_name Varchar (50),
+    document_date Datetime ,
+    year Int ,
+    term Int ,
+    PRIMARY KEY (document_id) 
+)
