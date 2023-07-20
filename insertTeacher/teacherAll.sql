9b1fe7920a052b889951392e439546348e8332a4
nantawat charoenrat <vss555mos@gmail.com>
2023-07-20 16:33:22 +0700

    20/07/2566/teacher

--- insertTeacher/teacherAll.sql
+++ insertTeacher/teacherAll.sql
@@ -1,95 +1,95 @@
 -- รองศาสตราจารย์วัฒนา พันธ์ลำเจียก
 -- admin
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("0","0","รองศาสตราจารย์","วัฒนา","พันธ์ลำเจียก","null","null",0);
+insert into teacher(`teacher_username`, `teacher_password`, `level_id`)
+    values ("admin2023","adminadmin",0);
 -- teacher 
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("1","1","รองศาสตราจารย์","วัฒนา","พันธ์ลำเจียก","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("2","2","รองศาสตราจารย์","วัฒนา","พันธ์ลำเจียก",1);
 
 -- ผู้ช่วยศาสตราจารย์พัฒณ์รพี สุนันทพจน์
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("2","2","ผู้ช่วยศาสตราจารย์","พัฒณ์รพี","สุนันทพจน์","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("3","3","ผู้ช่วยศาสตราจารย์","พัฒณ์รพี","สุนันทพจน์",1);
 
 -- ผู้ช่วยศาสตราจารย์ ดร.อิฐอารัญ ปิติมล
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("3","3","ผู้ช่วยศาสตราจารย์ ดร.","อิฐอารัญ","ปิติมล","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("4","4","ผู้ช่วยศาสตราจารย์ ดร.","อิฐอารัญ","ปิติมล",1);
 
 -- ผู้ช่วยศาสราจารย์ณัชติพงศ์ อูทอง
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("4","4","ผู้ช่วยศาสตราจารย์","ณัชติพงศ์","อูทอง","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("5","5","ผู้ช่วยศาสตราจารย์","ณัชติพงศ์","อูทอง",1);
 
 -- ผู้ช่วยศาสตราจารย์มาโนช ประชา
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("5","5","ผู้ช่วยศาสตราจารย์","มาโนช","ประชา","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("6","6","ผู้ช่วยศาสตราจารย์","มาโนช","ประชา",1);
 
 -- ผู้ช่วยศาสตราจารย์ ดร.ศิริชัย เตรียมล้ำเลิศ
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("6","6","ผู้ช่วยศาสตราจารย์ ดร.","ศิริชัย","เตรียมล้ำเลิศ","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("7","7","ผู้ช่วยศาสตราจารย์ ดร.","ศิริชัย","เตรียมล้ำเลิศ",1);
 
 -- ผู้ช่วยศาสตราจารย์เดชรัชต์ ใจถวิล
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("7","7","ผู้ช่วยศาสตราจารย์","เดชรัชต์","ใจถวิล","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("8","8","ผู้ช่วยศาสตราจารย์","เดชรัชต์","ใจถวิล",1);
 
 -- ผู้ช่วยศาสตราจารย์วีระชัย แย้มวจี
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("8","8","ผู้ช่วยศาสตราจารย์","วีระชัย","แย้มวจี","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("9","9","ผู้ช่วยศาสตราจารย์","วีระชัย","แย้มวจี",1);
 
 -- ผู้ช่วยศาสตราจารย์เจษฎา อรุณฤกษ์
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("9","9","ผู้ช่วยศาสตราจารย์","เจษฎา","อรุณฤกษ์","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("10","10","ผู้ช่วยศาสตราจารย์","เจษฎา","อรุณฤกษ์",1);
 
 -- รองศาสตราจารย์นชิรัตน์ ราชบุรี
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("10","10","รองศาสตราจารย์","นชิรัตน์","ราชบุรี","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("11","11","รองศาสตราจารย์","นชิรัตน์","ราชบุรี",1);
 
 -- ผู้ช่วยศาสตราจารย์สมรรถชัย จันทรัตน์
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("11","11","ผู้ช่วยศาสตราจารย์","สมรรถชัย","จันทรัตน์","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("12","12","ผู้ช่วยศาสตราจารย์","สมรรถชัย","จันทรัตน์",1);
 
 -- อาจารย์สิทธิ รักถนอม
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("12","12","อาจารย์","สิทธิ","รักถนอม","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("13","13","อาจารย์","สิทธิ","รักถนอม",1);
 
 -- ผู้ช่วยศาสตราจารย์ ดร.พฤศยน นินทนาวงศา
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("13","13","ผู้ช่วยศาสตราจารย์ ดร.","พฤศยน","นินทนาวงศา","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("14","14","ผู้ช่วยศาสตราจารย์ ดร.","พฤศยน","นินทนาวงศา",1);
 
 -- ผู้ช่วยศาสตราจารย์ ดร.ธนสิน บุญนาม
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("14","14","ผู้ช่วยศาสตราจารย์ ดร.","ธนสิน","บุญนาม","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("15","15","ผู้ช่วยศาสตราจารย์ ดร.","ธนสิน","บุญนาม",1);
 
 -- ผู้ช่วยศาสตราจารย์ ดร.ปอลิน กองสุวรรณ
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("15","15","ผู้ช่วยศาสตราจารย์ ดร.","ปอลิน","กองสุวรรณ","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("16","16","ผู้ช่วยศาสตราจารย์ ดร.","ปอลิน","กองสุวรรณ",1);
 
 -- ดร.พิชยพัชยา ศรีคร้าม
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("16","16","ดร.","พิชยพัชยา","ศรีคร้าม","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("17","17","ดร.","พิชยพัชยา","ศรีคร้าม",1);
 
 -- ดร.สุทินัน พรอนุรักษ์
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("17","17","ดร.","สุทินัน","พรอนุรักษ์","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("18","18","ดร.","สุทินัน","พรอนุรักษ์",1);
 
 -- ดร.กิตติวัณณ์ นิ่มเกิดผล
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("18","18","ดร.","กิตติวัณณ์","นิ่มเกิดผล","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("19","19","ดร.","กิตติวัณณ์","นิ่มเกิดผล",1);
 
 -- ดร.วินัย วิชัยพาณิชย์
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("19","19","ดร.","วินัย","วิชัยพาณิชย์","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("20","20","ดร.","วินัย","วิชัยพาณิชย์",1);
 
 -- อาจารย์วีระ คมปรียารัตน์
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("20","20","อาจารย์","วีระ","คมปรียารัตน์","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("21","21","อาจารย์","วีระ","คมปรียารัตน์",1);
 
 -- อาจารย์สมชาติ หรั่งเจริญ
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("21","21","อาจารย์","สมชาติ","หรั่งเจริญ","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("22","22","อาจารย์","สมชาติ","หรั่งเจริญ",1);
 
 -- ผู้ช่วยศาสตราจารย์พรรณราย รักษ์งาร
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("22","22","ผู้ช่วยศาสตราจารย์","พรรณราย","รักษ์งาร","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("23","23","ผู้ช่วยศาสตราจารย์","พรรณราย","รักษ์งาร",1);
 
 -- ผู้ช่วยศาสตราจารย์พัชราภัณฑ์ ไชยสังข์
-insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `email`, `phone`, `level_id`)
-    values ("23","23","ผู้ช่วยศาสตราจารย์","พัชราภัณฑ์","ไชยสังข์","null","null",1);
+insert into teacher(`teacher_username`, `teacher_password`, `position`, `firstname`, `lastname`, `level_id`)
+    values ("24","24","ผู้ช่วยศาสตราจารย์","พัชราภัณฑ์","ไชยสังข์",1);
