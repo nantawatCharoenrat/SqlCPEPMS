a1e5612ff0bba772f894f9e1deb080ea64bdb9ce
nantawat charoenrat <vss555mos@gmail.com>
2023-08-03 15:01:18 +0700

    03/08/2023/appoint

--- appoint.sql
+++ appoint.sql
@@ -1,8 +1,9 @@
 -- 14 new
 CREATE TABLE appoint (
     appoint_id Int NOT NULL AUTO_INCREMENT ,
-    title Varchar (50) ,
+    title Varchar (300) ,
     description Mediumtext ,
+    appoint_date Datetime ,
     group_id Int ,
     PRIMARY KEY (appoint_id) , 
     FOREIGN KEY (group_id) REFERENCES groups(group_id)
