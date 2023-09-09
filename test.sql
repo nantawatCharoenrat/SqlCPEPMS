31ca1ccbc0832301c21578b7726104e31fcd0b5c
nantawat charoenrat <vss555mos@gmail.com>
2023-09-10 00:22:24 +0700

    10/09/2023/00:22

--- test.sql
+++ test.sql
@@ -1,7 +1,7 @@
 -- 9 new
 CREATE TABLE test (
     test_id Int NOT NULL AUTO_INCREMENT ,
-    score Int ,
+    score FLOAT ,
     grade Varchar (2) ,
     student_id Varchar (13) ,
     PRIMARY KEY (test_id) , 
