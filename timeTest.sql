e277d639a039990cf0351f3b9344c88138e22c95
nantawat charoenrat <vss555mos@gmail.com>
2023-10-04 17:03:12 +0700

    04/10/2023/17:03

--- timeTest.sql
+++ timeTest.sql
@@ -2,8 +2,8 @@
 CREATE TABLE timeTest (
     timeTest_id Int NOT NULL AUTO_INCREMENT ,
     timeTest_date Date ,
-    start_time Varchar (10) ,
-    stop_time Varchar (10) ,
+    start_time Time ,
+    stop_time Time ,
     room_number Varchar (10) ,
     project_id Varchar (8) ,
     PRIMARY KEY (timeTest_id) , 
